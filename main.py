ef3bb3e24e9eb18c7cba18f4dd5e4e87610efd65
DrakorissVere <anmunimei@gmail.com>
2024-02-20 19:24:05 +0200

    add letters counting

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+def count_letters(string):
+    string = string.lower()
+    letters = {}
+
+    for c in string:
+        if c in letters:
+            letters[c] += 1
+        else:
+            letters[c] = 1
+
+    return letters
+
+
 def count_words(string):
     words = string.split()
     return len(words)
@@ -7,6 +20,8 @@ def main():
     with open("books/frankenstein.txt") as f:
         contents = f.read()
         print(f"There is {count_words(contents)} words")
+        print("There are this amount of symbols:")
+        print(count_letters(contents))
 
 
 main()
